37624555224a15200ec0b21b859d15ee10da841c
Роман Макаров <romanmakarov@MacBook.N4>
2021-12-25 19:21:28 +0300

    hello world

---
+++ README.md
@@ -0,0 +1,22 @@
+# rce-endpoint
+A simple python http server to demonstrate RCE vulnerabilities.
+
+### Run
+```
+lomaha@ping-pong % python3 main.py -a=0.0.0.0 -p=80
+```
+### Use
+#### Client side:
+```
+lomaha@rce-endpoint % curl -G "http://127.0.0.1/ping?ip=8.8.8.8;ls"
+
+b'PING 8.8.8.8 (8.8.8.8): 56 data bytes\n64 bytes from 8.8.8.8: icmp_seq=0 ttl=111 time=5.806 ms\n64 bytes from 8.8.8.8: icmp_seq=1 ttl=111 time=7.035 ms\n64 bytes from 8.8.8.8: icmp_seq=2 ttl=111 time=17.179 ms\n\n--- 8.8.8.8 ping statistics ---\n3 packets transmitted, 3 packets received, 0.0% packet loss\nround-trip min/avg/max/stddev = 5.806/10.007/17.179/5.096 ms\n.git\nREADME.md\nmain.py\n'
+```
+#### Server side:
+```
+Server started http://127.0.0.1:80
+127.0.0.1 - - [25/Dec/2021 19:20:31] "GET /ping?ip=8.8.8.8;ls HTTP/1.1" 200 -
+path: '/ping'
+query_params: 'ip'
+quer_value: '8.8.8.8;ls'
+```
---
+++ main.py
@@ -0,0 +1,60 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+import argparse
+import os
+import subprocess
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200) 
+        self.send_header("Content-type", "text/html") 
+        self.end_headers()
+        url = urlparse(self.path)
+        result = processing_request(url.path, url.query)
+        self.wfile.write(bytes(f"{result}\n", "utf-8"))
+
+        
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--server_address', default=False, help='server_address')
+    parser.add_argument('-p', '--server_port', default=False, help='server_port')
+    script_arg = parser.parse_args()
+    return script_arg
+
+def ping_command(ip):
+    result = subprocess.check_output(f'ping -c 3 {ip}', stderr=subprocess.STDOUT, shell=True)
+    return result
+
+def print_info(request_path, query):
+    print(f"path: '{request_path}'")
+    print(f"query_params: '{query[0]}'")
+    print(f"quer_value: '{query[1]}'")
+
+def processing_request(request_path, request_query):
+    if request_path == "/ping":
+        query = request_query.split("=")
+        print_info(request_path, query)
+        if query[0] == "ip":
+            result = ping_command(query[1])
+    return result
+
+
+def main():
+    try:
+        script_arg = create_parser()
+
+        address_server = script_arg.server_address or "localhost"
+        server_port = int(script_arg.server_port) or 8080
+
+        simple_server = HTTPServer((address_server, server_port), MyServer)
+        print(f"Server started http://{address_server}:{server_port}")
+
+        simple_server.serve_forever()
+    except KeyboardInterrupt:
+        simple_server.server_close()
+        print("Server stopped.")
+
+if __name__ == "__main__":        
+    main()
